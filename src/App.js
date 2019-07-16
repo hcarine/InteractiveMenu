1d666ce72aeceafc3b544cca403a2b6e120da70d
hcarine <hammescarine@gmail.com>
2019-07-17 00:00:13 +0200

    all filter

--- src/App.js
+++ src/App.js
@@ -5,13 +5,23 @@ import Dishes from './Dishes';
 import TextField from '@material-ui/core/TextField';
 
 const verifyFood = (alergy,food)=>{
-  return food.toLowerCase().startsWith(alergy)
+  return food && food.toLowerCase().startsWith(alergy)
 }
+const verifyTypeFood = (alergy,type)=>{
+  return type.length > 0 && type.filter(i=>
+    verifyFood(alergy,i)
+  ).length > 0
+}
+
 
 const filterValue = (alergy, dishes) => {
   alergy = alergy.toLowerCase()
-  const filterAlergy = ingredients.filter(item=>
-    verifyFood(alergy,item.name))
+  const filterAlergy = ingredients.filter(item =>
+    !verifyFood(alergy,item.name)?
+    !verifyFood(alergy,item.variety)?
+    !verifyTypeFood(alergy,item.type)?
+    false : true : true: true
+    )
    .map(item=> item.id)
 
   const filterDisches = dishes.filter(item => 
