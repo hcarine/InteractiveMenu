32088719fc162ad9e4b7152dfad000bc35138458
hcarine <hammescarine@gmail.com>
2019-07-16 23:48:24 +0200

    first filter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { dishes } from './menu';
-import Dishes from './Dishes'
+import { dishes,ingredients } from './menu';
+import Dishes from './Dishes';
+import TextField from '@material-ui/core/TextField';
+
+const verifyFood = (alergy,food)=>{
+  return food.toLowerCase().startsWith(alergy)
+}
+
+const filterValue = (alergy, dishes) => {
+  alergy = alergy.toLowerCase()
+  const filterAlergy = ingredients.filter(item=>
+    verifyFood(alergy,item.name))
+   .map(item=> item.id)
+
+  const filterDisches = dishes.filter(item => 
+   item.ingredient.filter(item=>
+     filterAlergy.includes(item)
+  )
+  .length > 0)
+  return filterDisches
+
+}
 
 const  App = () => {
+  const [selectedDishes,setSelectedDishes]= useState(dishes)
+
+  const handleChange = (event) => {
+    setSelectedDishes(filterValue( event.target.value,dishes))
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,7 +37,12 @@ const  App = () => {
          Menu Interactive
         </h1>
       </header>
-      <Dishes dishes={dishes}/>
+      <TextField
+        id="Seach-alergy"
+        label="Search your alergy"
+        onChange={handleChange}
+      />
+      <Dishes dishes={selectedDishes}/>
     </div>
   );
 }
